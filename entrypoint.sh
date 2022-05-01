6677f7125a61af02a212184a586999c797270d3f
Boubaker Med amine <boubaker.am@gmail.com>
2022-05-01 14:20:18 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,49 @@
 #!/bin/bash
+
 set -e
-cd odoo
-if [ "$1" = 'odoo' ]; then
-    shift
-    exec ./odoo-bin \
-        "--db_host=$DB_HOST" \
-        "--db_port=$DB_PORT" \
-        "--db_user=$DB_USER" \
-        "--db_password=$DB_PASSWORD" \
-        $ODOO_EXTRA_ARGS "$@"
-elif [ "$1" = 'debug' ]; then
-    shift
-    exec /usr/bin/python3 -m debugpy --listen 0.0.0.0:3001 ./odoo-bin \
-        "--db_host=$DB_HOST" \
-        "--db_port=$DB_PORT" \
-        "--db_user=$DB_USER" \
-        "--db_password=$DB_PASSWORD" \
-        $ODOO_EXTRA_ARGS "$@"
-else
-    exec "$@"
+
+if [ -v PASSWORD_FILE ]; then
+    PASSWORD="$(< $PASSWORD_FILE)"
 fi
+
+# set the postgres database host, port, user and password according to the environment
+# and pass them as arguments to the odoo process if not present in the config file
+: ${HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
+: ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
+: ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
+: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
+
+DB_ARGS=()
+function check_config() {
+    param="$1"
+    value="$2"
+    if grep -q -E "^\s*\b${param}\b\s*=" "$ODOO_RC" ; then       
+        value=$(grep -E "^\s*\b${param}\b\s*=" "$ODOO_RC" |cut -d " " -f3|sed 's/["\n\r]//g')
+    fi;
+    DB_ARGS+=("--${param}")
+    DB_ARGS+=("${value}")
+}
+check_config "db_host" "$HOST"
+check_config "db_port" "$PORT"
+check_config "db_user" "$USER"
+check_config "db_password" "$PASSWORD"
+
+case "$1" in
+    -- | odoo)
+        shift
+        if [[ "$1" == "scaffold" ]] ; then
+            exec odoo "$@"
+        else
+            wait-for-psql.py ${DB_ARGS[@]} --timeout=30
+            exec odoo "$@" "${DB_ARGS[@]}"
+        fi
+        ;;
+    -*)
+        wait-for-psql.py ${DB_ARGS[@]} --timeout=30
+        exec odoo "$@" "${DB_ARGS[@]}"
+        ;;
+    *)
+        exec "$@"
+esac
+
+exit 1
